e2ef85debee1ab2b0bd395174727e683d2544606
DefteZ <deftez@gmail.com>
2015-04-24 00:38:35 +0300

    додав заготовку описа і першої функції

---
+++ westra_osm_distance.py
@@ -0,0 +1,11 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Утиліта для полегшення імпорта данних
+Written by Andriy Danyleiko and <добав себе тут>
+Send bugs to andrii.danyleiko@gmail.com
+'''
+
+def distance_between_points():
+    '''порахувать відстань між двома точками, бажано в метрах, '''
+    pass
