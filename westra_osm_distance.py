eade6e3d93db51a1c17ba55f49e6e493d9703522
Skitalets <skitalets@gmail.com>
2015-04-24 23:06:17 +0300

    Розрахунок відстані за теоремою Піфагора

--- westra_osm_distance.py
+++ westra_osm_distance.py
@@ -6,6 +6,15 @@ Written by Andriy Danyleiko and Liudmyla Kislitsyna
 Send bugs to andrii.danyleiko@gmail.com
 '''
 
-def distance_between_points():
-    '''порахувать відстань між двома точками, бажано в метрах, '''
-    pass
+import math
+
+def distance_between_points(lat1, lon1, lat2, lon2):
+    lat1 = float(sys.argv[1])
+    lon1 = float(sys.argv[2])
+    lat2 = float(sys.argv[3])
+    lon2 = float(sys.argv[4])
+    L_lon = 111135
+    L_lat = 85300
+    delta_lat = math.fabs(lat2 - lat1)
+    delta_lon = math.fabs(lon2 - lon1)
+    return math.sqrt((delta_lat * L_lon) ** 2 + (delta_lon * L_lat) ** 2)
