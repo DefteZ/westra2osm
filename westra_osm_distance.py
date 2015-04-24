c7c8452f512d2f5fa8502b82c1ad18e147c74268
Skitalets <skitalets@gmail.com>
2015-04-24 23:42:18 +0300

    Виправила

--- westra_osm_distance.py
+++ westra_osm_distance.py
@@ -9,12 +9,10 @@ Send bugs to andrii.danyleiko@gmail.com
 import math
 
 def distance_between_points(lat1, lon1, lat2, lon2):
-    lat1 = float(sys.argv[1])
-    lon1 = float(sys.argv[2])
-    lat2 = float(sys.argv[3])
-    lon2 = float(sys.argv[4])
-    L_lon = 111135
-    L_lat = 85300
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
     delta_lat = math.fabs(lat2 - lat1)
     delta_lon = math.fabs(lon2 - lon1)
-    return math.sqrt((delta_lat * L_lon) ** 2 + (delta_lon * L_lat) ** 2)
+    return math.sqrt(delta_lat ** 2 + delta_lon ** 2)
