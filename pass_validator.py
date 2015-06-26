359e346d3b7614d4986f1c4238f51712ddae82d4
DefteZ <deftez@gmail.com>
2015-06-26 21:28:04 +0300

    moved functions to lib

--- pass_validator.py
+++ pass_validator.py
@@ -1,11 +1,11 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 import sys, argparse
-import urllib
 
-import fastkml, lxml.html
+import lxml.html
 
 import westra2osm_lib
+from westra2osm_lib import *
 
 def main():
     parser = createParser()
@@ -23,8 +23,9 @@ def main():
         poly = tuple(poly)
     
     print poly
+    bbox = poly2bbox(poly)
     
-    westra_kml_passes = get_pass_westra(poly)
+    westra_kml_passes = get_pass_westra(*bbox)
     for p in westra_kml_passes:
         root = lxml.html.fromstring(p.description)
         rows = root.xpath("table")[0].findall("tr")
@@ -45,19 +46,6 @@ def createParser():
     return parser
 
 
-def poly2bbox(poly):
-    '''обчислюємо крайні точки полігона'''
-    lats = [point[1] for point in poly]
-    lons = [point[0] for point in poly]
-    
-    longitude_west = min(lons) #долгота левой границы 
-    longitude_east = max(lons) #долгота правой границы
-    latitude_north = max(lats) #широта верхней границы
-    latitude_south = min(lats) #широта нижней границы
-    
-    return round(longitude_west,6), round(latitude_south,6), round(longitude_east,6), round(latitude_north,6)
-
-
 def calculete_tiles():
     raise NotImplementedError
 
@@ -68,39 +56,6 @@ def deg2num(lat_deg, lon_deg, zoom):
   xtile = int((lon_deg + 180.0) / 360.0 * n)
   ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
   return (xtile, ytile)
-
-
-def get_pass_westra(poly):
-    '''Функція для витягування геопривязаних перевалів із Вестри в kml xml формат.'''
-    url = 'http://westra.ru/passes/kml/passes.php'
-    values = {'BBOX' : '{0},{1},{2},{3}'.format(*poly2bbox(poly))}
-    #data = urllib.urlencode(values)
-    r_url = url + '?BBOX=' + values['BBOX']
-    response = urllib.urlopen(r_url)
-    if response.getcode() != 200:
-        print 'westra.ru return not 200 HTTP code'
-        sys.exit(100)
-    k = fastkml.kml.KML()
-    k.from_string(response.read())
-    features = list(k.features())
-    if len(features) == 1:
-        features = features[0]
-    else:
-        raise NotImplementedError('There are more than 1 feature in object')
-    ff = list(features.features())
-    placemarks = []
-    for folder in ff:
-        if isinstance(folder,fastkml.kml.Folder):
-            for p in folder.features():
-                placemarks.append(p)
-        else:
-            raise NotImplementedError('There are not fastkml.kml.Folder object in list')
-    
-    
-    #req = urllib2.Request(url, data)
-    #response = urllib2.urlopen(req)
-    #the_page = response.read()
-    return placemarks
 
 
 def get_pass_overpass():
@@ -116,24 +71,6 @@ class Pass():
     self.name
 '''
 
-def point_inside_polygon(x,y,poly):
-    '''determine if a point is inside a given polygon or not
-    Polygon is a list of (x,y) pairs.'''
-    n = len(poly)
-    inside =False
-    
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
-                    if p1y != p2y:
-                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xinters:
-                        inside = not inside
-        p1x,p1y = p2x,p2y
-    
-    return inside
+
 
 main()
--- westra2osm_lib.py
+++ westra2osm_lib.py
@@ -8,7 +8,9 @@ Send bugs to andrii.danyleiko@gmail.com
 
 import math
 import sys
+import urllib
 
+import fastkml
 import overpy
 
 
@@ -53,6 +55,74 @@ def distance_between_points(lat1, lon1, lat2, lon2):
     delta_lat = math.fabs(lat2 - lat1)
     delta_lon = math.fabs(lon2 - lon1)
     return math.sqrt(delta_lat ** 2 + delta_lon ** 2)
+
+
+def point_inside_polygon(x,y,poly):
+    '''determine if a point is inside a given polygon or not
+    Polygon is a list of (x,y) pairs.'''
+    n = len(poly)
+    inside =False
+    
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+    
+    return inside
+
+
+def poly2bbox(poly):
+    '''обчислюємо крайні точки полігона'''
+    lats = [point[1] for point in poly]
+    lons = [point[0] for point in poly]
+    
+    longitude_west = min(lons) #долгота левой границы 
+    longitude_east = max(lons) #долгота правой границы
+    latitude_north = max(lats) #широта верхней границы
+    latitude_south = min(lats) #широта нижней границы
+    
+    return round(longitude_west,6), round(latitude_south,6), round(longitude_east,6), round(latitude_north,6)
+
+
+def get_pass_westra(longitude_west, latitude_south, longitude_east, latitude_north):
+    '''Функція для витягування геопривязаних перевалів із Вестри в kml xml формат.
+    poly will be converted to bbox'''
+    url = 'http://westra.ru/passes/kml/passes.php'
+    values = {'BBOX' : '{0},{1},{2},{3}'.format(longitude_west, latitude_south, longitude_east, latitude_north)}
+    #data = urllib.urlencode(values)
+    r_url = url + '?BBOX=' + values['BBOX']
+    response = urllib.urlopen(r_url)
+    if response.getcode() != 200:
+        print 'westra.ru return not 200 HTTP code'
+        sys.exit(100)
+    k = fastkml.kml.KML()
+    k.from_string(response.read())
+    features = list(k.features())
+    if len(features) == 1:
+        features = features[0]
+    else:
+        raise NotImplementedError('There are more than 1 feature in object')
+    ff = list(features.features())
+    placemarks = []
+    for folder in ff:
+        if isinstance(folder,fastkml.kml.Folder):
+            for p in folder.features():
+                placemarks.append(p)
+        else:
+            raise NotImplementedError('There are not fastkml.kml.Folder object in list')
+    
+    
+    #req = urllib2.Request(url, data)
+    #response = urllib2.urlopen(req)
+    #the_page = response.read()
+    return placemarks
 
 
 if __name__ == "__main__":
